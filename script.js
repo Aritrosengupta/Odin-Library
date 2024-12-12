a0d9b7f47f40e324712e092f88b0914e6d091013
Aritro Sengupta <aritro.as@gmail.com>
2024-12-12 13:42:05 +0530

    Created a for loop to display books

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const MyLibrary=[];
+const MyLibrary=[
+    {Name:'Aritro',Author:'JRR',Pages:250,Read:'No'},
+    {Name:'Aritro',Author:'JRR',Pages:250,Read:'No'},
+    {Name:'Aritro',Author:'JRR',Pages:250,Read:'No'},
+    {Name:'Aritro',Author:'JRR',Pages:250,Read:'No'}
+];
 
 function Book(Name,Author,Pages,Status){
     this.Name=Name;
@@ -14,3 +19,15 @@ function Book(Name,Author,Pages,Status){
 function addBookToLibrary(obj){
     MyLibrary.push(obj);
 }
+
+const cardContainer=document.querySelector('.card-container');
+
+MyLibrary.forEach(book=>{
+    const NewCard=document.createElement('div');
+    NewCard.className='card';
+    const newName=document.createElement('h1');
+    newName.textContent=book.Name;
+    NewCard.appendChild(newName);
+
+    cardContainer.appendChild(NewCard);
+});
